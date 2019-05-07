21e794beaba35fd423b4f7b0f117be20954ee01e
Steve Minor <stminor@sbgtv.com>
2019-05-07 12:36:42 -0700

    complete weather app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,14 +1,62 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
-app.use(express.static(path.join(__dirname, "../public")));
+// setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Steve Minor'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Steve Minor'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Steve Minor',
+        message: 'You\'re on your own'
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
         forecast: 'It is 50 degrees.',
         location: 'Seattle'
+    });
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: 'Page not found!',
+        name: 'Steve Minor',
+        message: 'Help article not found.'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: 'Page not found!',
+        name: 'Steve Minor',
+        message: 'Whoops!'
     });
 });
 
